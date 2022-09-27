cb7b45ca02fcb994babfe8570449dfe067690dca
Александр Миронов <Mironovshsa@mail.ru>
2022-09-27 14:50:50 +0300

    Second rout

--- index.py
+++ index.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/api/<id>/')
+def user_profile(user_login):
+    return "Привет, #{}. Введите следующие данные:".format(user_login)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
